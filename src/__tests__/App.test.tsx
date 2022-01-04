0c1f66855b118eed71d03f7f6880e428ffc2b1a8
davidvdev <david@davidvdev.com>
2022-01-04 13:38:54 -0600

    removed boilerplate

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -7,23 +7,23 @@ import App from '../App'
 
 // Tests
 
-test('Renders main page correctly', async () => {
-    // Setup
-    render(<App />)
-    const buttonCount = await screen.findByRole('button')
-    const codeCount = await screen.queryByText(/The count is now:/)
+// test('Renders main page correctly', async () => {
+//     // Setup
+//     render(<App />)
+//     const buttonCount = await screen.findByRole('button')
+//     const codeCount = await screen.queryByText(/The count is now:/)
 
-    // Pre Expectations
-    expect(buttonCount.innerHTML).toBe('count is: 0')
-        // Instead of:
-        // expect(codeCount).toBeNull()
-        expect(codeCount).not.toBeInTheDocument()
+//     // Pre Expectations
+//     expect(buttonCount.innerHTML).toBe('count is: 0')
+//         // Instead of:
+//         // expect(codeCount).toBeNull()
+//         expect(codeCount).not.toBeInTheDocument()
 
-    // Init
-    user.click(buttonCount)
-    user.click(buttonCount)
+//     // Init
+//     user.click(buttonCount)
+//     user.click(buttonCount)
 
-    // Post Expectations
-    expect(buttonCount.innerHTML).toBe('count is: 2')
-    expect( await screen.queryByText(/The count is now:/)).toBeInTheDocument()
-})
+//     // Post Expectations
+//     expect(buttonCount.innerHTML).toBe('count is: 2')
+//     expect( await screen.queryByText(/The count is now:/)).toBeInTheDocument()
+// })
