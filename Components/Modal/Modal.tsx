a6d6e7a4fd5b0b9505d9deb946f9f91fefe1bf93
davidvdev <david@davidvdev.com>
2022-01-05 15:42:15 -0600

    first iteration of random text

--- Components/Modal/Modal.tsx
+++ Components/Modal/Modal.tsx
@@ -7,14 +7,36 @@ interface Props {
 
 const Modal = (props: Props) => {
 
+    const t = props.target
+
     const clickArea = (event:any) => {
         if (event.target === event.currentTarget){
             props.setModalOpen(false)
         }
     }
 
-    const randomizer = (name: string) => {
-        console.log(name)
+    const randomizer = (arr: Array<string>) => {
+        return(
+            <span style={{color: "red"}}>{arr[Math.floor(Math.random() * arr.length)]}</span>
+        )
+    }
+
+    const textGen = (name: string) => {
+        switch (name) {
+            case "Containers":
+                return(
+                    <div>
+                        <p>
+                            There are {randomizer(t.quantity)} {randomizer(t.description)} {randomizer(t.type)} nearby.
+                        </p>    
+                        <p>
+                            They contain {randomizer(t.content)}.
+                        </p>
+                    </div>
+                )
+            default:
+                break;
+        }
     }
 
     return(
@@ -22,8 +44,8 @@ const Modal = (props: Props) => {
         <div className="Modal" onClick={(event) => {clickArea(event)}}>
             <div className='content'>
                 <button onClick={() => {props.setModalOpen(false)}}>click to close</button>
-                <h1>The {props.target.name}</h1>
-                { randomizer(props.target.name) }
+                <h1>The {t.name}</h1>
+                { textGen(t.name) }
             </div>
         </div>
     )
