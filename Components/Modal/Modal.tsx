23fb1ba225bbf0441e2e0275c80ec5b5ba46e151
davidvdev <david@davidvdev.com>
2022-01-04 20:57:31 -0600

    click oustide modal closes modal

--- Components/Modal/Modal.tsx
+++ Components/Modal/Modal.tsx
@@ -8,8 +8,16 @@ interface Props {
 }
 
 const Modal = (props: Props) => {
+
+    const clickArea = (event:any) => {
+        if (event.target === event.currentTarget){
+            props.setModalOpen(false)
+        }
+    }
+
     return(
-        <div className="Modal" onClick={() => {props.setModalOpen(false)}}>
+        // <div className="Modal" onClick={() => {props.setModalOpen(false)}}>
+        <div className="Modal" onClick={(event) => {clickArea(event)}}>
             <div className='content'>
                 <button onClick={() => {props.setModalOpen(false)}}>click to close</button>
                 <h1>Hello {props.data.name}</h1>
